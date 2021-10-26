fd77c1adde6b3b94632e0f5d5914160ad85ea706
Victor Campuzano <victor@vicampuzano.com>
2021-10-26 21:49:11 +0200

    getFromAmplitude command

--- app/Console/Commands/getFromAmplitude.php
+++ app/Console/Commands/getFromAmplitude.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Aplistorical\SdAmplitude;
 use Illuminate\Console\Command;
 
 use app\Drivers\AmplitudeDownloader;
@@ -13,7 +14,8 @@ class getFromAmplitude extends Command
      *
      * @var string
      */
-    protected $signature = 'aplistorical:getFromAmplitude';
+    protected $signature = 'aplistorical:getFromAmplitude
+    {jobId : Job id to download}';
 
     /**
      * The console command description.
@@ -39,6 +41,11 @@ class getFromAmplitude extends Command
      */
     public function handle()
     {
+        $sdamp = new SdAmplitude($this->argument('jobId'));
+        $this->line("Ok, let's download all files ... It willl take a few minutes. Be patient.");
+        $this->line("Please, check the logs for debug info...");
+        $sdamp->getData();
+        $this->info("Process completed!");
         return Command::SUCCESS;
     }
 }
